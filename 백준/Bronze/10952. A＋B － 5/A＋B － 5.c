59c14d1c35279762420fd9d432a4da90b93935f0
Huijae99 <163639710+JJang0603@users.noreply.github.com>
2025-04-04 12:32:52 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int A, B;
+
+	for (;;)
+	{
+		scanf("%d %d", &A, &B);
+
+		if ((A == 0) & (B == 0))
+		{
+			break;
+		}
+		else
+		{
+			printf("%d\n", (A + B));
+		}
+	}
+
+	return 0;
+}
